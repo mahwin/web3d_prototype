e76815e5d67be6b6961572ed213f2c250c64c501
mahwin <mahwin7085@gmail.com>
2025-02-28 16:37:19 +0900

    init

---
+++ src/rackInfo.ts
@@ -0,0 +1,134 @@
+const device_1 = {
+    x: 1,
+    y: 10,
+    z: 1,
+    uPosition:10,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+const device_2 = {
+    x: 1,
+    y: 2,
+    z: 1,
+    uPosition: 12,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+
+const device_3 = {
+    x: 1,
+    y: 10,
+    z: 1,
+    uPosition:22,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+const device_4 = {
+    x: 1,
+    y: 2,
+    z: 1,
+    uPosition:24,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+
+const device_5 = {
+    x: 1,
+    y: 2,
+    z: 0.8,
+    uPosition:26,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+
+const device_6 = {
+    x: 1,
+    y: 1,
+    z: 0.4,
+    uPosition:28,
+    railsUsed:'back',
+    orientation:'back',
+}
+
+const device_7 = {
+    x: 1,
+    y: 2,
+    z: 0.9,
+    uPosition:30,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+const device_8 = {
+    x: 1,
+    y: 1,
+    z: 0.4,
+    uPosition:31,
+    railsUsed:'back',
+    orientation:'back',
+}
+
+const device_9 = {
+    x: 1,
+    y: 1,
+    z: 0.4,
+    uPosition:40,
+    railsUsed:'back',
+    orientation:'back',
+}
+
+const device_10 ={
+    x: 1,
+    y: 1,
+    z: 0.6,
+    uPosition:41,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+
+const device_11 = {
+    x: 1,
+    y: 1,
+    z: 0.4,
+    uPosition:42,
+    railsUsed:'back',
+    orientation:'back',
+}
+
+const device_12 ={
+    x: 1,
+    y: 1,
+    z: 0.6,
+    uPosition:43,
+    railsUsed:'front',
+    orientation:'front',
+}
+
+const device_13 = {
+    x: 1,
+    y: 1,
+    z: 0.4,
+    uPosition:44,
+    railsUsed:'back',
+    orientation:'back',
+}
+
+const device_14 = {
+    x: 1,
+    y: 1,
+    z: 0.05,
+    uPosition:45,
+    railsUsed:'back',
+    orientation:'back',
+}
+
+
+const divices = [device_1, device_2, device_3, device_4, device_5, device_6, device_7, device_8, device_9, device_10, device_11, device_12, device_13, device_14];
+
+export default divices;
